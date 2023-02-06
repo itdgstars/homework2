9301d6bb5c05d7a82e58161057ea2708b72e32a6
SergeyK <it-dgstars@mail.ru>
2023-02-06 11:13:16 +0300

    Решили вторую задачу

---
+++ z2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write ("Введите любое число: ");
+int Rn = Convert.ToInt32(Console.ReadLine());
+if (Rn / 100 > 0)
+{
+
+  while (Rn > 1000)
+  {
+  Rn = Rn / 10;
+  
+   }
+
+   Rn = Rn % 10;
+   Console.WriteLine ("Третье число: " + Rn);
+}
+else {
+  Console.WriteLine ("Третьей цифры нет.");
+}
