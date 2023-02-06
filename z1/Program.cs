8e33c69b8de5edd0681cb6276daa8cfd3d9f9b2d
SergeyK <it-dgstars@mail.ru>
2023-02-06 11:11:57 +0300

    Решили первую задачу

---
+++ z1/Program.cs
@@ -0,0 +1,10 @@
+﻿int Number()
+{
+  Console.Write ("Введите трехзначное число: ");
+int Rn = Convert.ToInt32(Console.ReadLine());
+  int num1 = Rn / 10;
+  int num2 = num1 % 10;
+  return num2;
+}
+int res = Number();
+Console.WriteLine("Вторая цифра числа: " + res);
