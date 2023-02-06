82f79705e67d1c0c675308e6b5dcaf765f65c4ee
SergeyK <it-dgstars@mail.ru>
2023-02-06 11:14:24 +0300

    Решили третью задачу

---
+++ z3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write ("Введите любое число: ");
+int Rn = Convert.ToInt32(Console.ReadLine());
+
+
+if (Rn > 0 & Rn < 8)
+{
+  if (Rn > 5 & Rn <8)
+  {
+    Console.WriteLine ("Да, это выходной день");
+  }
+  else {
+    Console.WriteLine ("Нет, это будний день!");
+  }
+}
+else {
+  Console.WriteLine ("Ваше число не является днем недели.");
+}
